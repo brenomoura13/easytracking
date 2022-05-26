4615eb88f9120c9b73013b764bdbc741f34551c5
Breno Moura <59323654+brenomoura13@users.noreply.github.com>
2022-05-25 21:40:28 -0300

    upd: Corrigir responsividade.

--- src/components/Loading.js
+++ src/components/Loading.js
@@ -7,8 +7,8 @@ const Loading = () => {
       <div className="relative font-medium h-screen flex items-center content-center justify-center">
         <div className="mr-auto ml-auto w-full">
           <div className="w-full max-w-md mr-auto ml-auto mt-4 mb-1 text-center">
-            <p className="text-6xl font-montserrat font-semibold text-violet-600 text-sm py-1 text-center rounded text-center focus:outline-none">             
-              <FontAwesomeIcon icon={faCircleNotch} className="no-underline text-5xl mr-5" spin/>Carregando
+            <p className="font-montserrat font-semibold text-violet-600 text-4xl md:text-6xl py-1 text-center rounded text-center focus:outline-none">             
+              <FontAwesomeIcon icon={faCircleNotch} className="no-underline text-3xl md:text-5xl mr-5" spin/>Carregando
             </p>
           </div>
         </div>
