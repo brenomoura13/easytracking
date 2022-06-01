d4c1718f64d7a328fb8afca5b05a4e68458da95a
Breno Moura <59323654+brenomoura13@users.noreply.github.com>
2022-05-31 21:42:00 -0300

    fix: Pequeno ajuste na responsividade

--- src/components/ContinueButton.js
+++ src/components/ContinueButton.js
@@ -10,7 +10,7 @@ const ContinueButton = () => {
           <div className="w-full max-w-md mr-auto ml-auto mt-4 mb-1 text-center">
             <Link href="/auth">
               <button type="button" className="font-montserrat font-semibold text-violet-600 focus:outline-none">             
-                  <a className="no-underline text-4xl md:text-6xl">Continuar</a><p><FontAwesomeIcon icon={faArrowDown} className="no-underline text-3xl md:text-5xl"/></p>
+               <a className="no-underline text-4xl md:text-6xl">Continuar<p><FontAwesomeIcon icon={faArrowDown} className="no-underline text-3xl md:text-5xl"/></p></a>
               </button>
             </Link>
           </div>
--- src/components/Loading.js
+++ src/components/Loading.js
@@ -7,7 +7,7 @@ const Loading = () => {
       <div className="relative font-medium h-screen flex items-center content-center justify-center">
         <div className="mr-auto ml-auto w-full">
           <div className="w-full max-w-md mr-auto ml-auto mt-4 mb-1 text-center">
-            <p className="font-montserrat font-semibold text-violet-600 text-4xl md:text-6xl py-1 text-center rounded text-center focus:outline-none">             
+            <p className="font-montserrat font-semibold text-violet-600 text-4xl md:text-6xl py-1 rounded text-center focus:outline-none">             
               <FontAwesomeIcon icon={faCircleNotch} className="no-underline text-3xl md:text-5xl mr-5" spin/>Carregando
             </p>
           </div>
